b0aad05370fc19fb79e84c9beed6bf4391b79d5a
hamitkorkmaz0344 <hakorkmaz.2003@gmail.com>
2023-12-13 00:57:20 +0300

    update

--- diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.cs
+++ diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.OleDb;
 namespace diyetisyen_otomasyonu
 {
     public partial class Form1 : Form
@@ -15,6 +15,41 @@ namespace diyetisyen_otomasyonu
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        OleDbConnection con ;
+        OleDbCommand cmd;
+        OleDbDataReader dr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ad = textBox1.Text;
+            string sifre=textBox2.Text;
+            con=new OleDbConnection("Provider=Microsoft.ACE.oledb.12.0;Data Source=diyetisyen.accdb");
+            cmd = new OleDbCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText="SELECT*FROM login where adsoyad='"+textBox1.Text + "' AND sifre= '" +textBox2.Text + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read() )
+            {
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("bilgileriniz hatalı tekrar deneyiniz");
+            }
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
---
+++ diyetisyen otomasyonu/diyetisyen otomasyonu/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diyetisyen_otomasyonu
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.TopLevel = false;
+            form3.AutoScroll= true;
+            panel2.Controls.Add(form3);
+            form3.Show();
+        }
+    }
+}
