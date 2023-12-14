2db0f008de601a37cb858206a9140b428dffb0e4
hamitkorkmaz0344 <hakorkmaz.2003@gmail.com>
2023-12-14 14:29:49 +0300

    UPDATE

--- diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.Designer.cs
+++ diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.Designer.cs
@@ -34,65 +34,215 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Şifremi_unuttum = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_tcNo = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sifregir = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(228, 109);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.Size = new System.Drawing.Size(239, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "adınız ve soyadınız ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 178);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(128, 142);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.Size = new System.Drawing.Size(97, 29);
             this.label2.TabIndex = 1;
             this.label2.Text = "sifrenız";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(445, 106);
+            this.textBox1.Location = new System.Drawing.Point(283, 70);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(445, 172);
+            this.textBox2.Location = new System.Drawing.Point(283, 149);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.Size = new System.Drawing.Size(212, 22);
+            this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(283, 305);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(17, 269);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
+            this.button1.Size = new System.Drawing.Size(102, 69);
+            this.button1.TabIndex = 3;
             this.button1.Text = "giris";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(489, 305);
+            this.button2.Location = new System.Drawing.Point(30, 369);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 4;
             this.button2.Text = "cıkıs";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::diyetisyen_otomasyonu.Properties.Resources.images;
+            this.pictureBox1.Location = new System.Drawing.Point(370, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::diyetisyen_otomasyonu.Properties.Resources.manisa_turgutlu_diyetisyen_ve_diyetisyen_fiyatlari_530;
+            this.pictureBox2.Location = new System.Drawing.Point(548, 44);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(240, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Footlight MT Light", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(574, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "diyetisyen merkezimize hoşgeldiniz sağlıklı bir yaşam için sizlerin hizmetindeyiz" +
+    "";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(144, 211);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Beni hatırla";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Şifremi_unuttum
+            // 
+            this.Şifremi_unuttum.AutoSize = true;
+            this.Şifremi_unuttum.Location = new System.Drawing.Point(201, 278);
+            this.Şifremi_unuttum.Name = "Şifremi_unuttum";
+            this.Şifremi_unuttum.Size = new System.Drawing.Size(101, 16);
+            this.Şifremi_unuttum.TabIndex = 9;
+            this.Şifremi_unuttum.TabStop = true;
+            this.Şifremi_unuttum.Text = "şifremi_unuttum!\r\n";
+            this.Şifremi_unuttum.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Şifremi_unuttum_LinkClicked);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.sifregir);
+            this.panel1.Controls.Add(this.txt_tcNo);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(335, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 293);
+            this.panel1.TabIndex = 10;
+            this.panel1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(623, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "x";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txt_tcNo
+            // 
+            this.txt_tcNo.Location = new System.Drawing.Point(232, 35);
+            this.txt_tcNo.MaxLength = 11;
+            this.txt_tcNo.Name = "txt_tcNo";
+            this.txt_tcNo.Size = new System.Drawing.Size(157, 22);
+            this.txt_tcNo.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "tamam";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // sifregir
+            // 
+            this.sifregir.Location = new System.Drawing.Point(232, 182);
+            this.sifregir.Name = "sifregir";
+            this.sifregir.Size = new System.Drawing.Size(157, 22);
+            this.sifregir.TabIndex = 1;
+            this.sifregir.Visible = false;
+            this.sifregir.TextChanged += new System.EventHandler(this.sifregir_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(5, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "tc kimlik numaranızı giriniz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(69, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 22);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "şifreniz=";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Şifremi_unuttum);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -102,6 +252,10 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +269,18 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.LinkLabel Şifremi_unuttum;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox sifregir;
+        private System.Windows.Forms.TextBox txt_tcNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.cs
+++ diyetisyen otomasyonu/diyetisyen otomasyonu/Form1.cs
@@ -12,6 +12,7 @@ namespace diyetisyen_otomasyonu
 {
     public partial class Form1 : Form
     {
+        string baglantı = "Provider=Microsoft.ACE.oledb.12.0;Data Source=diyetisyen.accdb";
         public Form1()
         {
             InitializeComponent();
@@ -19,22 +20,26 @@ namespace diyetisyen_otomasyonu
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            if (Properties.Settings.Default.textBox1 != string.Empty)
+            {
+                textBox1.Text = Properties.Settings.Default.textBox1;
+                textBox2.Text = Properties.Settings.Default.textBox2;
+            }
         }
-        OleDbConnection con ;
+        OleDbConnection con;
         OleDbCommand cmd;
         OleDbDataReader dr;
         private void button1_Click(object sender, EventArgs e)
         {
             string ad = textBox1.Text;
-            string sifre=textBox2.Text;
-            con=new OleDbConnection("Provider=Microsoft.ACE.oledb.12.0;Data Source=diyetisyen.accdb");
+            string sifre = textBox2.Text;
+            con = new OleDbConnection(baglantı);
             cmd = new OleDbCommand();
             con.Open();
             cmd.Connection = con;
-            cmd.CommandText="SELECT*FROM login where adsoyad='"+textBox1.Text + "' AND sifre= '" +textBox2.Text + "'";
+            cmd.CommandText = "SELECT*FROM login where adsoyad='" + textBox1.Text + "' AND sifre= '" + textBox2.Text + "'";
             dr = cmd.ExecuteReader();
-            if (dr.Read() )
+            if (dr.Read())
             {
                 Form2 f2 = new Form2();
                 f2.Show();
@@ -50,6 +55,62 @@ namespace diyetisyen_otomasyonu
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+
+        {
+            if (checkBox2.Checked)
+            {
+                Properties.Settings.Default.textBox1 = textBox1.Text;
+                Properties.Settings.Default.textBox2 = textBox2.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Şifremi_unuttum_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+        string sifre;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            using (OleDbConnection con = new OleDbConnection(baglantı))
+            {
+                con.Open();
+                string tc = txt_tcNo.Text;
+                string sorgu = $"SELECT sifre FROM login WHERE tc = '{tc}'";
+                using (OleDbCommand cmd = new OleDbCommand(sorgu, con))
+                {
+                  //  cmd.Parameters.AddWithValue("@tc", tc);
+                    using (OleDbDataReader oku = cmd.ExecuteReader())
+                    {
+                        if (oku.Read())
+                        {
+                            sifre = oku["sifre"].ToString();
+                            MessageBox.Show("SİFRENİZ " + sifre);
+                        }
+                        else
+                        {
+                            MessageBox.Show(" HATA");
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        private void sifregir_TextChanged(object sender, EventArgs e)
+        {
+            sifregir.Visible = false;
         }
     }
 }
